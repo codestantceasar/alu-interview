8a33ee4717cdbf669d3b9a43eeb2b150d49d35a9
codestantceasar <c.akas@alustudent.com>
2025-05-19 20:12:04 +0200

    Add minOperations implementation and test cases

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+
+def minOperations(n):
+    """Calculate the fewest number of operations to get n 'H' characters."""
+    if n < 2:
+       return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
+
+
+if __name__ == "__main__":
+     n = 4
+     print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    
+     n = 12
+     print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
